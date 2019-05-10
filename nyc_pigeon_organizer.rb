6fd69703c4a08a90665d59edfa4826de53af6294
Christopher Yuan <chrisyuan7@gmail.com>
2019-05-10 17:18:49 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |att, info|
+    info.each do |item, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][att] ||= []
+        pigeon_list[name][att] << item.to_s
+      end
+    end
+  end
+  pigeon_list
 end
